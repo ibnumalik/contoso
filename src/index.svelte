ea2dab7b0ce02d151971ad60c96954021b2b5365
Shaiful <shaiful.malik@myeg.com.my>
2020-05-13 18:06:16 +0800

    Add store to save recipients member

--- src/index.svelte
+++ src/index.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
   import tailwind from './tailwind.svelte';
   import Products from './components/products.svelte';
+  import {user} from './stores';
 
   const members = ['associate', 'platinum', 'diamond'];
+  let userMember = '';
+
+  function register(selected) {
+    user.update(user => selected);
+  }
+
+  user.subscribe(user => userMember = user)
 </script>
 
 <div class="container mx-auto">
@@ -15,13 +23,15 @@
   </header>
 
   <section class="mb-20 members">
-    <h2 class="mb-8 text-2xl">Continue as:</h2>
+    <h2 class="mb-8 text-2xl">Continue as: {userMember}</h2>
 
     <ul class="flex">
       {#each members as member}
       <li class="flex-1 px-4 first:pl-0 last:pr-0">
+
         <button
-          class="rounded bg-blue-600 px-4 py-6 uppercase font-bold text-center text-blue-200 w-full hover:bg-blue-700"
+          class="{member === userMember ? 'bg-indigo-600' : ''} rounded bg-blue-600 px-4 py-6 uppercase font-bold text-center text-blue-200 w-full hover:bg-blue-700"
+          on:click={() => register(member)}
         >
           {member}
         </button>
