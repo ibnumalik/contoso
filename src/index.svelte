5c5785bf5b7e6687dc1ab9f8b429b0831768bce0
Shaiful <shaiful.malik@myeg.com.my>
2020-05-13 16:37:08 +0800

    Add tailwind component base

--- src/index.svelte
+++ src/index.svelte
@@ -1,23 +1,7 @@
 <script lang="ts">
-  const a: string = "Svelte",
-    b: string = "Typescript",
-    c: string = "Rollup";
+  import tailwind from './tailwind.svelte';
 </script>
 
-<style lang="scss">
-  :global(body) {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    margin: 0;
-  }
 
-  h1 {
-    font: normal 24pt Lato, "Helvetica Neue", Arial, Helvetica, sans-serif;
-    color: dimgray;
-    cursor: default;
-  }
-</style>
 
-<h1>{a} + {b} + {c}</h1>
+<h1 class="">Hello Svelte</h1>
